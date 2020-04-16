45d3dcad3900504cf4d27ef820c529baf88e2772
eduwr <eduwric@gmail.com>
2020-04-16 20:47:21 -0300

    add environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
+import { TypeOrmConfigService } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TasksModule,
+    // TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,29 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'eduardo',
-  password: 'edu306022',
-  database: 'taskmanagement',
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true,
-};
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: process.env.TYPEORM_HOST,
+//   port: parseInt(process.env.TYPEORM_PORT),
+//   username: process.env.TYPEORM_USERNAME,
+//   password: process.env.TYPEORM_PASSWORD,
+//   database: process.env.TYPEORM_DATABASE,
+//   entities: [__dirname + '/../**/*.entity.{js,ts}'],
+//   synchronize: true,
+// };
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: true,
+    };
+  }
+}
